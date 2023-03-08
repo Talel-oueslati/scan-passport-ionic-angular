c3fbc2cdcf273b9b3c67448e1b5b5837192e60bd
talel <yonkoshanks11@gmail.com>
2023-03-08 17:05:07 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'scan-passeport',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
